fdbc80c79b085c449f83b554cbb4dec7e9438f04
PersesTitan <ToramPersesTitan@gmail.com>
2022-05-31 08:43:18 +0900

    commit

---
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
