232af0011005fb801e81ed9e57fc7d49d8a34559
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-01 21:32:07 +0900

    add Controller

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!");
+        return "hello";
+    }
+}
