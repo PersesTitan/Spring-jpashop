6cec4cf0bb69eb76070cab19e34edc4bbe5e7de0
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-02 00:17:51 +0900

    repository

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -2,6 +2,21 @@ package jpabook.jpashop;
 
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
