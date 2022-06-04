b31f0e011723cc466121c6a30d2f7d51359d4e0a
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-05 01:10:12 +0900

    Test 코드 생성

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,6 +21,7 @@ public class MemberServiceTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Rollback(false)
     public void 회원가입() {
         //given
         Member member = new Member();
