bc94a1609fb44fb2f0a361290e0b57cd7b40c152
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-11 00:01:54 +0900

    Lombok 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,18 +1,19 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
-
-    protected Address() {}
 }
