0478f3ee9e1f7ba7beff7f89decc96bc43982482
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-12 15:32:23 +0900

    create NotEnoughStockException

---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockException.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockException extends RuntimeException {
+
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -3,11 +3,14 @@ package jpabook.jpashop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Scanner;
+
 @SpringBootTest
 class JpashopApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
