53431abb5b8b0af1784aabe70ac00aaf889fbb99
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-03 22:03:13 +0900

    HomeController 생성

---
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+}
