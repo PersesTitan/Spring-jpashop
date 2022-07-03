203e789888f7b4156b9f1de1ff3d51da6fca8926
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-03 22:03:47 +0900

    MemberController 생성

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm memberForm) {
+        return null;
+    }
+}
