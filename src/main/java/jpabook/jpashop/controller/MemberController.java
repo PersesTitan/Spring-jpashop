b39e47ca7b1a9517e1b61979e969dcd3574886ff
PersesTitan <ToramPersesTitan@gmail.com>
2022-07-03 23:53:22 +0900

    사용하지 않는 로직 삭제

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -2,12 +2,14 @@ package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -48,5 +50,4 @@ public class MemberController {
         model.addAttribute("members", members);
         return "members/memberList";
     }
-
 }
